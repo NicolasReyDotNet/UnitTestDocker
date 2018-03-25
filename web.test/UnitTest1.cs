8d8f793a6c6a03960fbe6066a6a00d4f3d567820
Nicolas <nicolasrey@laposte.net>
2018-03-25 17:58:27 +0200

    Init

---
+++ web.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace web.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.NotEmpty(new List<string> { "test" });
+        }
+    }
+}
